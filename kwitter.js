e0301d309b539d0bbd0a2ac17e6491a5ac25f166
Durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-04-12 18:32:09 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,6 @@
+function addUser(){
+   var d= document.getElementById("username").value;
+   localStorage.setItem("username",document);
+   console.log("kuku")
+   window.location="kwitter_room.html";
+}
